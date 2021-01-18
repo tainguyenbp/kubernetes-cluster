758f4ec9126ba5e6dce92627788db78717cb0d3e
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 10:56:11 +0700

    Update install-kubernetes-master1.sh

--- install-kubernetes-master1.sh
+++ install-kubernetes-master1.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR"/env-hosts.sh
 
-echo "$IP_K8S_MASTER	$NAME_K8S_MASTER" >> /etc/hosts
-echo "$IP_K8S_NODE_1	$NAME_K8S_NODE_1" >> /etc/hosts
-echo "$IP_K8S_NODE_2	$NAME_K8S_NODE_2" >> /etc/hosts
+echo "$IP_K8S_MASTER	$NAME_K8S_MASTER" >> "$PATH_FILE_HOST_NODE"
+echo "$IP_K8S_NODE_1	$NAME_K8S_NODE_1" >> "$PATH_FILE_HOST_NODE"
+echo "$IP_K8S_NODE_2	$NAME_K8S_NODE_2" >> "$PATH_FILE_HOST_NODE"
 
