d9a3f3474da874864376e5f57024447aa453d8cf
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:10:01 +0700

    Update nstall-kubernetes-node2.sh

--- nstall-kubernetes-node2.sh
+++ nstall-kubernetes-node2.sh
@@ -35,3 +35,6 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
 EOF
 
 yum install -y kubelet-1.18.14 kubeadm-1.18.14 kubectl-1.18.14
+
+systemctl start docker && systemctl enable docker
+systemctl start kubelet && systemctl enable kubelet
