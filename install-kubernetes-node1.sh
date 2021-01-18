52a59bd1c4887039dd64025ffca1271c46230d49
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:02:26 +0700

    Update install-kubernetes-node1.sh

--- install-kubernetes-node1.sh
+++ install-kubernetes-node1.sh
@@ -18,3 +18,5 @@ sed -i '/swap/d' /etc/fstab
 swapoff -a
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
