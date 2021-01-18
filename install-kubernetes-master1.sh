b504be1d9e0722c7644a86c4e74bc59b672b3ae8
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:08:32 +0700

    Update install-kubernetes-master1.sh

--- install-kubernetes-master1.sh
+++ install-kubernetes-master1.sh
@@ -33,3 +33,5 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
+
+yum install -y kubelet-1.18.14 kubeadm-1.18.14 kubectl-1.18.14
