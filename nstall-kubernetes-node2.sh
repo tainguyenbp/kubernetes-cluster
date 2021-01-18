19cc5f0f6c74a7514830f086eb5a69c0ba598a24
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:06:23 +0700

    Update nstall-kubernetes-node2.sh

--- nstall-kubernetes-node2.sh
+++ nstall-kubernetes-node2.sh
@@ -20,3 +20,5 @@ swapoff -a
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce-18.06.3.ce-3.el7 docker-ce-cli-18.06.3.ce-3.el7 containerd.io
