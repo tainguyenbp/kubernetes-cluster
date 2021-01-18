cd6222d5348a53291dc3fd9cc2799dc9b00ca74a
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:10:39 +0700

    Update nstall-kubernetes-node2.sh

--- nstall-kubernetes-node2.sh
+++ nstall-kubernetes-node2.sh
@@ -34,6 +34,8 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 yum install -y kubelet-1.18.14 kubeadm-1.18.14 kubectl-1.18.14
 
 systemctl start docker && systemctl enable docker
