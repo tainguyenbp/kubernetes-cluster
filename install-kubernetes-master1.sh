77169c0f558e3ad9d272a0b657f5ae5ced6f7a7c
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 11:06:56 +0700

    Update install-kubernetes-master1.sh

--- install-kubernetes-master1.sh
+++ install-kubernetes-master1.sh
@@ -22,3 +22,14 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 yum install -y docker-ce-18.06.3.ce-3.el7 docker-ce-cli-18.06.3.ce-3.el7 containerd.io
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
