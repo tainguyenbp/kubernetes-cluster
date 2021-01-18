00e2b9453d2ef610a3d34ad7660901916c90f145
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-18 10:46:32 +0700

    Create env-hosts.sh

---
+++ env-hosts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NAME_K8S_MASTER='kubernetes-master'
+export IP_K8S_MASTER='192.168.10.1'
+
+export NAME_K8S_NODE_1='kubernetes-node1'
+export IP_K8S_NODE_1='192.168.10.10'
+
+export NAME_K8S_NODE_2='kubernetes-node2'
+export IP_K8S_NODE_2='192.168.10.20'
