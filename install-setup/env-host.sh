82490cb613afa65b377ee8ec1c9a95f9d939a6af
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-10-05 21:53:09 +0700

    Create env-host.sh

---
+++ install-setup/env-host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NAME_K8S_MASTER='k8s-master'
+export IP_K8S_MASTER='192.168.10.1'
+
+export NAME_K8S_NODE_1='k8s-node1'
+export IP_K8S_NODE_1='192.168.10.10'
+
+export NAME_K8S_NODE_2='k8s-node2'
+export IP_K8S_NODE_2='192.168.10.20'
+
+export PATH_FILE_HOST_NODE='/etc/hosts'
