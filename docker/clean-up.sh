ca687d30d51e1f69125bae7300d666824243dcc9
tainguyenbp <nguyenngoctaibp@gmail.com>
2022-06-15 23:16:00 +0700

    Create clean-up.sh

---
+++ docker/clean-up.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker rm -f $(docker ps -qa)
+docker volume rm $(docker volume ls -q)
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /opt/rke"
+for dir in $cleanupdirs; do
+  echo "Removing $dir"
+  rm -rf $dir
+done
