40312c9dcc31f7c8aa5ff1f82a0c433ccb04ca39
tainguyenbp <nguyenngoctaibp@gmail.com>
2022-08-15 11:33:30 +0700

    Update update-key-vaule.sh

--- hashicorp-vault/scripts/update-key-vaule.sh
+++ hashicorp-vault/scripts/update-key-vaule.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p "${CURRENT_DIR}"/env
+
+export VAULT_ADDR=''
+export VAULT_TOKEN=''
+
+GROUP=''
+PROJECT_NAME=''
+ENVIROMENT='prod'
+
+for ENVIROMENT in ${ENVIROMENT} ; do
+    VAULT_SECRET_PATH="kv-${GROUP}-${PROJECT_NAME}/${ENVIROMENT}"
+    vault kv put -format=json ${VAULT_SECRET_PATH} @${ENVIROMENT}-${PROJECT_NAME}.json
+done
