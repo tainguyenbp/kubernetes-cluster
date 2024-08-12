0cc3fab110f80ac8131e36f1de6d37b856bf5533
bm8 bmFtZQ <nguyenngoctaibp@gmail.com>
2024-08-12 13:26:12 +0700

    Create provider.tf

---
+++ azure/teraform/aks/modules/resource_group/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.6.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
