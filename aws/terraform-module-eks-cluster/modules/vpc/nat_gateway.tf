83436fbf78504aa7b21d1a29ae02f8c8454cd28c
bm8 bmFtZQ <nguyenngoctaibp@gmail.com>
2024-12-31 19:31:31 +0700

    Create nat_gateway.tf

---
+++ aws/terraform-module-eks-cluster/modules/vpc/nat_gateway.tf
@@ -0,0 +1,20 @@
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name        = "${var.environment}-nat-eip"
+    Environment = var.environment
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name        = "${var.environment}-nat"
+    Environment = var.environment
+  }
+}
