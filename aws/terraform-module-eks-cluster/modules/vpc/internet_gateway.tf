1d96a8188e82b95aa085466869e2f62ffe3b66eb
bm8 bmFtZQ <nguyenngoctaibp@gmail.com>
2024-12-31 19:30:24 +0700

    Create internet_gateway.tf

---
+++ aws/terraform-module-eks-cluster/modules/vpc/internet_gateway.tf
@@ -0,0 +1,9 @@
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
+  }
+}
