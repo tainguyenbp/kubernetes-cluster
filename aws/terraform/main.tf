f51caa74584fe199e374686170a06a408fc7feba
bm8 bmFtZQ <nguyenngoctaibp@gmail.com>
2025-01-05 10:15:03 +0700

    Update main.tf

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -1,17 +1,12 @@
-resource "aws_vpc" "main" {
-    cidr_block = "172.31.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-        Environment = "Dev"
-        Name = "vpc-dev"
-    }
-}
 
-resource "aws_subnet" "private_subnet_1a_rds_dev" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "172.31.0.0/24"
-    tags = {
-        Name = "private-subnet-1a-rds-dev"
-    }
+
+module "vpc_dev" {
+    source = "./modules/vpc"
+    cidr_block = "172.29.0.0/16"
+    environment = "development"
+}
+module "vpc_prod" {
+    source = "./modules/vpc"
+    cidr_block = "172.31.0.0/16"
+    environment = "production"
 }
